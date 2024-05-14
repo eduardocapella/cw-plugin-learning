ad4d64101fb6440a80f68ab33915cc94b98c156a
Eduardo Capella <eduardocapella@gmail.com>
2024-05-14 19:11:10 -0300

    Segundo ajuste

--- cw-plugin-learning.php
+++ cw-plugin-learning.php
@@ -1,5 +1,38 @@
 <?php
+/**
+ * Plugin Name: Plugin Learning
+ * Description: Capella's first plugin with Composer, Namespace and Autoload.
+ * Version: 1.0
+ * Author: Eduardo Capella
+**/
 
-if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
-    require __DIR__ . '/vendor/autoload.php';
+namespace CWPL;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
+
+// Include Composer's autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+function cw_testing_hooks() {
+    echo '<h1>Testing!!!</h1>';
+}
+add_action( 'in_admin_header', __NAMESPACE__ . '\cw_testing_hooks', 1 );
+
+
+// $articles = new Article();
+
+require_once __DIR__ . '/includes/cpts/Article.php';
+// if the class exists create a new instance of this class
+if ( class_exists( 'CWPL\CPTs\Article\Article' ) ) {
+    $the_article = new Article();
+} else {
+    echo "<h1>DOESN'T WORK!</h1>";
+}
+
+register_activation_hook( __FILE__, [ $the_article, 'activate' ] );
+
+register_deactivation_hook( __FILE__, [ $the_article, 'deactivate' ] );
---
+++ includes/cpts/Article.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CWPL\CPTs\Article;
+
+class Article {
+
+    function __construct() {
+        add_action( 'init', array( $this, 'cw_cpt_articles' ) );
+    }
+
+    // The custom function to register a Article post type
+    public function cw_cpt_articles() {
+
+        // Set the labels, this variable is used in the $args array
+        $labels = array(
+            'name'               => __( 'Articles' ),
+            'singular_name'      => __( 'Article' ),
+            'add_new'            => __( 'Add New Article' ),
+            'add_new_item'       => __( 'Add New Article' ),
+            'edit_item'          => __( 'Edit Article' ),
+            'new_item'           => __( 'New Article' ),
+            'all_items'          => __( 'All Articles' ),
+            'view_item'          => __( 'View Articles' ),
+            'search_items'       => __( 'Search Articles' )
+        );
+        
+        // The arguments for our post type, to be entered as parameter 2 of register_post_type()
+        $args = array(
+            'labels'            => $labels,
+            'description'       => 'Holds our Articles',
+            'public'            => true,
+            'menu_position'     => 6,
+            'supports'          => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
+            'has_archive'       => true,
+            'show_in_admin_bar' => true,
+            'show_in_nav_menus' => true,
+            'has_archive'       => true
+        );
+        
+        // Call the actual WordPress function
+        // Parameter 1 is a name for the post type
+        // $args array goes in parameter 2.
+        register_post_type( 'article', $args );
+    }
+
+}
